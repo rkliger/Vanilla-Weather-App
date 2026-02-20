d5d302b547937f324429c5118a06ab621884e813
rkliger <80487679+rkliger@users.noreply.github.com>
2026-02-20 13:02:09 -0700

    Updated date, image, CSS

--- SRC/script.js
+++ SRC/script.js
@@ -11,8 +11,11 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   {
+    iconElement.innerHTML = ` <img src="${response.data.condition.icon_url}" class="current-temperature"/>`;
+
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
