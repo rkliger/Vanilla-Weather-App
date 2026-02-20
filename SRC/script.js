39c3bfcd4d2381a136e99cea325e057e5ceb7e7f
rkliger <80487679+rkliger@users.noreply.github.com>
2026-02-20 12:25:10 -0700

    Added weather data

--- SRC/script.js
+++ SRC/script.js
@@ -5,9 +5,35 @@ function refreshWeather(response) {
   // Always display the canonical city name returned by the API,
   // regardless of how the user typed it.
   let cityElement = document.querySelector(".current-city");
-  if (cityElement) {
+  if (cityElement) console.log(response.data);
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  {
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
   }
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
